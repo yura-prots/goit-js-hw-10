82ff113dfab785ffb9e15cd7e6790dde815e2244
yura-prots <yuraprots@gmail.com>
2023-12-25 16:09:22 +0200

    change URL

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import axios from 'axios';
 
+const url = 'https://api.thecatapi.com/v1/breeds';
+
 axios.defaults.headers.common['x-api-key'] = process.env.API_KEY;
 
 axios
-  .get(
-    `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${process.env.API_KEY}`
-  )
-  .then(res => console.log(res))
+  .get(url)
+  .then(response => {
+    console.log(response);
+    return response;
+  })
   .catch(err => console.log(err));
