7ac4cbdeb967b7e9c97a11f2fb4cff0e92ad8ecd
yura-prots <yuraprots@gmail.com>
2023-12-26 14:24:08 +0200

    add SlimSelect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import 'dotenv/config';
 import axios from 'axios';
+import SlimSelect from 'slim-select';
 
-const url = 'https://api.thecatapi.com/v1/breeds';
+import 'slim-select/dist/slimselect.css';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+const refs = {
+  selectEl: document.querySelector('select.breed-select'),
+  loaderEl: document.querySelector('p.loader'),
+  errorEl: document.querySelector('p.error'),
+  catInfoEl: document.querySelector('.cat-info'),
+};
 
 axios.defaults.headers.common['x-api-key'] = process.env.API_KEY;
 
-axios
-  .get(url)
-  .then(response => {
-    console.log(response);
-    return response;
-  })
-  .catch(err => console.log(err));
+getData(`${BASE_URL}/breeds`);
+
+function getData(url) {
+  fetch(url)
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => console.log(err));
+}
+
+// .then(catList => {
+//   new SlimSelect({
+//     select: 'select.breed-select',
+//     data: catList,
+//   });
+// })
